a2bb7fa11b9ed8667474785e842a06cdb8fee9d6
Danny Culp <culpdanny@gmail.com>
2019-12-11 08:46:43 -0800

    working on 10.2

--- 10.py
+++ 10.py
@@ -16,38 +16,51 @@ for row in range(len(mapArr)):
 
 # print(asteroidCoords)
 
+asteroidCoords.remove((29, 28))
+
+
+def getOtherAsteroidMeasurementsSorted(station, otherAsteroids):
+    asteroidMeasurements = []
+    for asteroid in otherAsteroids:
+        dx = float(asteroid[0]) - station[0]
+        dy = float(asteroid[1]) - station[1]
+        if dx == 0:
+            if dy > 0:
+                angle = 270
+            else:
+                angle = 90
+        # elif dy == 0:
+        #     if dx > 0:
+        #         angle = "+zero"
+        #     else:
+        #         angle = "-zero"
+        else:
+            angle = math.degrees(numpy.arctan2(dy, dx))
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+        asteroidMeasurements.append((angle, distance))
+    return sorted(asteroidMeasurements, key=lambda x: x[1])
+
+
+def getVisibleAsteroidMeasurements(station, otherAsteroids):
+    visibleAsteroidMeasurements = otherAsteroids.copy()
+    j = 0
+    while j < len(visibleAsteroidMeasurements) - 1:
+        #asteroidVisible = True
+        for measurement in visibleAsteroidMeasurements[j+1:]:
+            if visibleAsteroidMeasurements[j][0] == measurement[0]:
+                visibleAsteroidMeasurements.remove(measurement)
+        j += 1
+    return visibleAsteroidMeasurements
+
+
 mostVisible = 0
 bestStation = None
 for i in range(len(asteroidCoords)):
-    asteroidMeasurements = []
-    potentialStation = asteroidCoords.pop(0)
-    for asteroid in asteroidCoords:
-        dx = float(asteroid[0]) - potentialStation[0]
-        dy = float(asteroid[1]) - potentialStation[1]
-        if dx == 0:
-            if dy > 0:
-                slope = 270
-            else:
-                slope = 90
-        # elif dy == 0:
-        #     if dx > 0:
-        #         slope = "+zero"
-        #     else:
-        #         slope = "-zero"
-        else:
-            slope = math.degrees(numpy.arctan2(dy, dx))
-        distance = math.sqrt(dx ** 2 + dy ** 2)
-        asteroidMeasurements.append((slope, distance))
 
-    visibleAsteroidMeasurements = []
-    asteroidMeasurements = sorted(asteroidMeasurements, key=lambda x: x[1])
-    j = 0
-    while j < len(asteroidMeasurements) - 1:
-        #asteroidVisible = True
-        for measurement in asteroidMeasurements[j+1:]:
-            if asteroidMeasurements[j][0] == measurement[0]:
-                asteroidMeasurements.remove(measurement)
-        j += 1
+    potentialStation = asteroidCoords.pop(0)
+    asteroidMeasurements = getOtherAsteroidMeasurementsSorted(
+        potentialStation, asteroidCoords)
+
     print(potentialStation, len(asteroidMeasurements))
     if len(asteroidMeasurements) > mostVisible:
         bestStation = potentialStation
