22b4c267747b88cdfa3038e3b3804bc9c0abb595
Danny <danny@blend.com>
2019-12-12 17:27:20 -0800

    working on 12.2

--- 12.py
+++ 12.py
@@ -1,10 +1,14 @@
-import math
+import sys
 
 
 class Moon:
     def __init__(self, position):
         self.position = position
+        self.start = position
         self.velocity = [0, 0, 0]
+        self.positionLog = []
+        self.velocityLog = []
+        # self.beenHereBefore = False
 
     def applyGravity(self, otherMoon):
         for i in range(3):
@@ -16,6 +20,18 @@ class Moon:
     def applyVelocity(self):
         for i in range(3):
             self.position[i] += self.velocity[i]
+        log = self.position.copy() + self.velocity.copy()
+        if log in self.log:
+            print(self.start, self.position, len(self.log))
+        else:
+            self.log.append(log)
+        # logInt = vectorToInt(
+        #     self.position.copy()+self.velocity.copy())
+        # if logInt not in self.log:
+        #     self.beenHereBefore = False
+        #     self.log.append(logInt)
+        # else:
+        #     self.beenHereBefore = True
 
     def getEnergy(self):
         potential = 0
@@ -26,13 +42,40 @@ class Moon:
         return potential * kinetic
 
 
-# moons = [[-1, 0, 2], [2, -10, -7], [4, -8, 8], [3, 5, -1]]
-moons = [[-9, 10, -1], [-14, -8, 14], [1, 5, 6], [-19, 7, 8]]
+# moons = [[-1, 0, 2], [2, -10, -7], [4, -8, 8],[3, 5, -1]]  # 2772 steps to repeat
+moons = [[-8, -10, 0], [5, 5, 10], [2, -7, 3], [9, -8, -3]]  # 4686774924
+# moons = [[-9, 10, -1], [-14, -8, 14], [1, 5, 6], [-19, 7, 8]]
 moons = [Moon(position) for position in moons]
 
-steps = 1000
+steps = 0
+combinedPVLog = []
 
-for i in range(steps):
+
+def getCombinedPVHash(moons):
+    h = []
+    for moon in moons:
+        h.append(moon.position.copy() + moon.velocity.copy())
+    return hash(str(h))
+
+
+def progressBar(value, endvalue, bar_length=20):
+
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(
+        arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+
+
+historyRepeats = False
+# for i in range(steps):
+while not historyRepeats:
+    log = getCombinedPVHash(moons)
+    combinedPVLog.append(log)
+
+    # individualRepeats = True
     for _ in range(len(moons)):
         currMoon = moons.pop(0)
         for otherMoon in moons:
@@ -40,11 +83,19 @@ for i in range(steps):
         moons.append(currMoon)
     for moon in moons:
         moon.applyVelocity()
-        # print(moon.position, moon.velocity)
+        # individualRepeats = individualRepeats and moon.beenHereBefore
+    steps += 1
+    # print(moon.position, moon.velocity)
     # print('\n')
+    if getCombinedPVHash(moons) in combinedPVLog:
+        print(steps, log)
+        historyRepeats = True
+    #progressBar(steps, 4686774924)
 
-totalEnergy = 0
-for moon in moons:
-    print(moon.position, moon.velocity)
-    totalEnergy += moon.getEnergy()
-print(totalEnergy)
+print(steps)
+
+# totalEnergy = 0
+# for moon in moons:
+#     print(moon.position, moon.velocity)
+#     totalEnergy += moon.getEnergy()
+# print(totalEnergy)
